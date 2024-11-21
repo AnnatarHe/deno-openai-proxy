8ccd66befa7c7dda8d9bb2c6aa8104e707136d82
AnnatarHe <annatar.he+github@gmail.com>
2024-11-22 00:34:00 +0800

    feat(gemini): add support for gemini

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 // https://github.com/justjavac/openai-proxy/blob/main/deno.ts
+
 const OPENAI_API_HOST = "api.openai.com";
+const GEMINI_API_HOST = "https://generativelanguage.googleapis.com";
 
 Deno.serve(async (request) => {
   const url = new URL(request.url);
-  url.host = OPENAI_API_HOST;
+
+  if (url.pathname.includes("gemini")) {
+    url.host = GEMINI_API_HOST;
+  } else {
+    url.host = OPENAI_API_HOST;
+  }
 
   const newRequest = new Request(url.toString(), {
     headers: request.headers,
